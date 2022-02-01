71e440d0c3a49cc96a051f736e32d3805db53a3e
zilat rida <redaovish200@hotmail.com>
2022-02-01 22:47:27 +0100

    first commit

---
+++ spring-exam-projet/src/main/java/com/examSpring/springexamprojet/SpringExamProjetApplication.java
@@ -0,0 +1,13 @@
+package com.examSpring.springexamprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExamProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExamProjetApplication.class, args);
+	}
+
+}
---
+++ spring-exam-projet/src/test/java/com/examSpring/springexamprojet/SpringExamProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examSpring.springexamprojet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
